2bf5022d0a691b45bc3a510de08781c217702538
Sanjay03K <75990210+Sanjay03K@users.noreply.github.com>
2022-11-18 21:56:48 +0530

    sprint 1 completed

---
+++ Project Development Phase/Sprint 1/app.py
@@ -0,0 +1,92 @@
+#code for login and register
+
+from distutils.log import debug
+from flask import Flask, render_template, request, redirect, url_for, session
+import ibm_db
+import re
+
+app = Flask(__name__)
+  
+app.secret_key = 'a'
+
+try:
+    conn = ibm_db.connect("DATABASE=BLUDB;HOSTNAME=b1bc1829-6f45-4cd4-bef4-10cf081900bf.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32304;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=xbj61988;PWD=pWF7JgHPSHKIYFoJ", '', '')
+    print("Connected to database: ", conn)
+except:
+    print("Unable to connect to the database")
+
+@app.route('/')    
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/loginpage',methods=['GET', 'POST'])
+def loginpage():
+    global userid
+    msg = ''
+
+    if request.method == 'POST' :
+        username = request.form['username']
+        password = request.form['password']
+        sql = "SELECT * FROM donors WHERE username =? AND password=?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.bind_param(stmt,2,password)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print (account)
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['USERNAME']
+            userid=  account['USERNAME']
+            session['username'] = account['USERNAME']
+            msg = 'Logged in successfully !'
+            
+            return redirect(url_for('dash'))
+        else:
+            msg = 'Incorrect username / password !'
+    return render_template('login.html', msg = msg)
+   
+@app.route('/registration')
+def home():
+    return render_template('register.html')
+
+@app.route('/register',methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if request.method == 'POST' :
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+        phone = request.form['phone']
+        city = request.form['city']
+        infect = request.form['infect']
+        blood = request.form['blood']
+        sql = "SELECT * FROM users WHERE username =?"
+        stmt = ibm_db.prepare(conn, sql)
+        ibm_db.bind_param(stmt,1,username)
+        ibm_db.execute(stmt)
+        account = ibm_db.fetch_assoc(stmt)
+        print(account)
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'name must contain only characters and numbers !'
+        else:
+            insert_sql = "INSERT INTO users VALUES (?, ?, ?, ?, ?, ?, ?)"
+            prep_stmt = ibm_db.prepare(conn, insert_sql)
+            ibm_db.bind_param(prep_stmt, 1, username)
+            ibm_db.bind_param(prep_stmt, 2, email)
+            ibm_db.bind_param(prep_stmt, 3, password)
+            ibm_db.bind_param(prep_stmt, 4, city)
+            ibm_db.bind_param(prep_stmt, 5, infect)
+            ibm_db.bind_param(prep_stmt, 6, blood)
+            ibm_db.bind_param(prep_stmt, 7, phone)
+            ibm_db.execute(prep_stmt)
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return render_template('register.html', msg = msg)
